3a803f3601c4b8715017c1d442eb772900ff1c96
Artem Valyakn <valyakin@gmail.com>
2019-08-26 17:38:49 +0500

    move grammar parsing functionality to ocore

--- src/errors/lib/OjsonParsingError.js
+++ src/errors/lib/OjsonParsingError.js
@@ -1,4 +1,4 @@
-export default class UndefinedGrammar extends Error {
+export default class OjsonParsingError extends Error {
 	constructor (message) {
 		super(message)
 		Error.captureStackTrace(this, this.constructor)
--- src/pages/lib/EditorPage/EditorPage.js
+++ src/pages/lib/EditorPage/EditorPage.js
@@ -1,35 +1,10 @@
 import MonacoEditor from 'vue-monaco'
-import isArray from 'lodash/isArray'
 import debounce from 'lodash/debounce'
 import get from 'lodash/get'
 import { mapActions, mapState, mapGetters } from 'vuex'
 import monacoLanguages from 'src/languages'
 import { AgentControls } from 'src/components'
 import { ValidationError, ParsingError } from 'src/errors'
-
-const TYPES = {
-	IF: 'IF',
-	INT: 'INT',
-	STR: 'STR',
-	APP: 'APP',
-	MAIN: 'MAIN',
-	INIT: 'INIT',
-	PAIR: 'PAIR',
-	TRUE: 'TRUE',
-	CASE: 'CASE',
-	CASES: 'CASES',
-	FALSE: 'FALSE',
-	STATE: 'STATE',
-	ARRAY: 'ARRAY',
-	OBJECT: 'OBJECT',
-	DECIMAL: 'DECIMAL',
-	FORMULA: 'FORMULA',
-	PAYLOAD: 'PAYLOAD',
-	MESSAGE: 'MESSAGE',
-	MESSAGES: 'MESSAGES',
-	BOUNCE_FEES: 'BOUNCE_FEES',
-	BOUNCE_ASSET: 'BOUNCE_ASSET'
-}
 
 /* eslint-disable-next-line no-undef */
 const config = __APP_CONFIG__
@@ -113,7 +88,6 @@ export default {
 	},
 	methods: {
 		...mapActions({
-			parseOscript: 'grammars/parseOscript',
 			parseOjson: 'grammars/parseOjson',
 			validateAa: 'grammars/validateOjson',
 
@@ -134,8 +108,7 @@ export default {
 
 			if (this.code !== '') {
 				try {
-					const parserResult = await this.parseOjson(this.code)
-					this.serializedOjson = await this.serializeOjson(parserResult)
+					this.serializedOjson = JSON.stringify(await this.parseOjson(this.code))
 				} catch (e) {
 					this.openResultPane()
 					this.resultMessage = e.message
@@ -162,8 +135,8 @@ export default {
 		},
 		async validate () {
 			this.resultMessage = ''
-
 			await this.codeChanged()
+
 			if (this.serializedOjson !== '') {
 				this.openResultPane()
 				try {
@@ -176,99 +149,6 @@ export default {
 					this.resultMessage = e.message
 				}
 			}
-		},
-		async serializeOjson (parserResult) {
-			if (!isArray(parserResult)) {
-				throw new Error('parserResult should be Array')
-			}
-			if (parserResult.length !== 1) {
-				throw new Error('parserResult should be Array of length 1')
-			}
-			return JSON.stringify(['autonomous agent', await this.processTree(parserResult[0])])
-		},
-		async processTree (tree) {
-			if (tree.type === TYPES.MAIN) {
-				return this.processAsObject(tree)
-			} else if (tree.type === TYPES.MESSAGES) {
-				return { messages: await this.processTree(tree.value) }
-			} else if (tree.type === TYPES.ARRAY) {
-				return this.processAsArray(tree)
-			} else if (tree.type === TYPES.CASES) {
-				return { cases: await this.processAsArray(tree) }
-			} else if (tree.type === TYPES.CASE) {
-				return this.processAsObject(tree)
-			} else if (tree.type === TYPES.BOUNCE_FEES) {
-				const fees = {}
-				for (let i = 0; i < tree.value.length; i++) {
-					const asset = tree.value[i]
-					if (fees.hasOwnProperty(asset.asset)) throw new Error(`Duplicate asset '${asset.asset}' at line ${asset.context.line} col ${asset.context.col}`)
-					fees[asset.asset] = await this.processTree(asset.value)
-				}
-				return { bounce_fees: fees }
-			} else if (tree.type === TYPES.INT) {
-				return tree.value
-			} else if (tree.type === TYPES.STR) {
-				return tree.value
-			} else if (tree.type === TYPES.TRUE) {
-				return tree.value
-			} else if (tree.type === TYPES.FALSE) {
-				return tree.value
-			} else if (tree.type === TYPES.DECIMAL) {
-				return tree.value
-			} else if (tree.type === TYPES.FORMULA) {
-				const formula = tree.value
-				try {
-					await this.parseOscript(formula)
-					return '{' + formula + '}'
-				} catch (e) {
-					const msg = e.message
-					const match = msg.match(/invalid syntax at line ([\d]+) col ([\d]+):([\s\S]+)/m)
-					if (match) {
-						throw new Error(`Invalid formula syntax at line ${tree.context.line + Number(match[1]) - 1} col ${tree.context.col + Number(match[2]) - 1}:${match[3]}`)
-					} else {
-						throw new Error(`Invalid formula starting at line ${tree.context.line} col ${tree.context.col}`)
-					}
-				}
-			} else if (tree.type === TYPES.INIT) {
-				return { init: await this.processTree(tree.value) }
-			} else if (tree.type === TYPES.MESSAGE) {
-				return this.processAsObject(tree)
-			} else if (tree.type === TYPES.PAYLOAD) {
-				return { payload: await this.processTree(tree.value) }
-			} else if (tree.type === TYPES.OBJECT) {
-				return this.processAsObject(tree)
-			} else if (tree.type === TYPES.IF) {
-				return { if: await this.processTree(tree.value) }
-			} else if (tree.type === TYPES.STATE) {
-				return { state: await this.processTree(tree.value) }
-			} else if (tree.type === TYPES.APP) {
-				return { app: tree.value }
-			} else if (tree.type === TYPES.PAIR) {
-				return { [await this.processTree(tree.key)]: await this.processTree(tree.value) }
-			} else {
-				throw new Error(`Unknown type ${tree.type}`)
-			}
-		},
-		async processAsObject (tree) {
-			const obj = {}
-			for (let i = 0; i < tree.value.length; i++) {
-				const st = tree.value[i]
-				const res = await this.processTree(st)
-				const key = Object.keys(res)[0]
-				const value = Object.values(res)[0]
-				if (obj.hasOwnProperty(key)) throw new Error(`Duplicate key '${key}' at line ${st.context.line} col ${st.context.col}`)
-				obj[key] = value
-			}
-			return obj
-		},
-		async processAsArray (tree) {
-			const arr = []
-			for (let i = 0; i < tree.value.length; i++) {
-				const st = tree.value[i]
-				const res = await this.processTree(st)
-				arr.push(res)
-			}
-			return arr
 		},
 		async handleTemplateSelect (event) {
 			const selected = event.target.value
